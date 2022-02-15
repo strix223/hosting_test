40f0718c34a1ae24123ed507942f8aac2992cea8
Pieter van Laer <r3xrabb1t@gmail.com>
2022-02-15 16:22:45 -0500

    cleaned up code base; added picture modal; fixed bug when clicking gallery image titles

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+// VARS FOR NAV BAR LINK FUNCTIONALITY
 const pageLinks = document.querySelectorAll('.link-item')
 
+// VARS FOR LOADING THE PLAYER
+const playerPlaceholder = document.getElementById('player-placeholder')
+
+// VARS FOR PLAYER INSTRUCTION FUNCTIONALITY
+const toast = document.getElementById('toast-container')
+const toastClose = document.getElementById('toast-close')
+
+// VARS FOR SCROLLING AND WINDOW NAVIGATION
+const section = document.getElementById('section-home')
+const sections = document.querySelectorAll('.page-section')
+const topbar = document.getElementById('topbar')
+
+// VARS FOR GALLERY MODAL FUNTIONALITY
+const modal = document.getElementById('img-modal')
+const images = document.querySelectorAll('.img-div')
+const modalClose = document.getElementById('modal-close')
+
+// VARS FOR SCHEDULE SECTION FUNCTIONALITY
 const scheduleItems = document.querySelectorAll('.schedule-item')
 const infoDivs = document.querySelectorAll('.schedule-info')
 const iframes = document.querySelectorAll('.iframe')
 
-const toast = document.querySelector('.toast-container')
-const toastClose = document.getElementById('toast-close')
-
-const section = document.getElementById('section-home')
-const sections = document.querySelectorAll('.pageSection')
-const topbar = document.getElementById('topbar')
-
-const playerPlaceholder = document.getElementById('player-placeholder')
 
 let current_index = 0
 
+// ADD EVENT LISTENERS FOR EACH NAVBAR LINK
 pageLinks.forEach((link, idx) => {
    
     link.addEventListener('click', () => {
@@ -25,17 +37,25 @@ pageLinks.forEach((link, idx) => {
     
 })
 
-function setActiveLink(linkElement) {
-    pageLinks.forEach((link, idx) => {
-        if(idx !== current_index) {
-            link.classList.remove('link-active')
-
-        }
+// ADD FUNCTIONALITY FOR DISPLAYING THE IMAGE MODAL WHEN AN IMAGE IS CLICKED
+images.forEach(image => {
+    image.addEventListener('click', (e) => {
+        modal.classList.add('modal-active')
+        const modalImg = modal.querySelector('img.modal-img')
+        const clickedImg = image.querySelector('img.img')
+        modalImg.src = clickedImg.src
     })
-    linkElement.classList.add('link-active')
-}
+})
 
+// CLOSE THE MODAL WHEN BUTTON CLICKED
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('modal-active')
+})
+
+// EVENT LISTENERS AND FUCNCTIONALITY FOR SCHEDULING PAGE
 scheduleItems.forEach((item, idx) => {
+
+    //DEACTIVATE ALL IFRAMES
     iframes.forEach(iframe => {
         iframe.classList.remove('iframe-active')
 
@@ -43,6 +63,7 @@ scheduleItems.forEach((item, idx) => {
 
 
 
+    // IF MOUSE OVER SCHEDULE ITEM, REMOVE ACTIVE CLASS FROM OTHERS AND ADD ACTIVE TO CURRENT
     item.addEventListener('mouseenter', () => {
         scheduleItems.forEach(i => {
             i.classList.remove('schedule-item-active')
@@ -63,10 +84,11 @@ scheduleItems.forEach((item, idx) => {
     
 })
 
+// UPDATE THE NAVBAR LINK DECORATION DEPENDING ON SCOLL LOCATION ON THE PAGE
 window.addEventListener('scroll', () => {
+    
+    // IF SCROLL POSITION IS WITHIN SECTION, SET THE CORRESTPONDING NAVBAR LINK
     let middle = scrollY + window.innerHeight / 2
-
-    console.log(middle)
 
     sections.forEach((section, idx) => {
         if(middle > section.offsetTop && middle < section.offsetTop + (section.offsetHeight - 200)) {
@@ -75,6 +97,7 @@ window.addEventListener('scroll', () => {
         }
     })
 
+    // IF SCROLL POSITION IS BEYOND THE FIRST PAGE, SHRINK THE NAVBAR
     if (scrollY > (section.getBoundingClientRect().height - 40)) {
         topbar.classList.add('topbar-small')
     } else {
@@ -82,7 +105,10 @@ window.addEventListener('scroll', () => {
     }
 })
 
+// IF HERO PLACEHOLDER IS CLICKED, LOAD THE UNITY WEBPLAYER AND APPEND TO DOCUMENT
 playerPlaceholder.addEventListener('click', () => {
+
+    // CREATE CANVAS ELEMENT WITH ATTRIBUTES
     const canvas = document.createElement('canvas')
     canvas.id = "unity-canvas"
     canvas.width = 960
@@ -91,9 +117,11 @@ playerPlaceholder.addEventListener('click', () => {
     canvas.style.height = 600
     canvas.style.background = "url('Build/Build2.jpg') center / cover" 
 
+    // APPEND CANVAS AND REMOVE PLACEHOLDER
     section.appendChild(canvas)
     section.removeChild(playerPlaceholder)
 
+    // LOAD THE WEBPLAYER 
     createUnityInstance(document.querySelector("#unity-canvas"), {
         dataUrl: "Build/Build.data",
         frameworkUrl: "Build/Build.framework.js",
@@ -102,74 +130,38 @@ playerPlaceholder.addEventListener('click', () => {
         companyName: "stonefromnothing",
         productName: "jazz-band",
         productVersion: "0.1",
-        // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
-        // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
       });
 
+    // PREVENT THE WEBPLAYER FROM CAPTURING MOUSE SCROLL
     document.addEventListener('wheel', onScroll, false)
     document.addEventListener('mousemove', onMouse, false)
     var content = document.getElementById('unity-canvas')
     function onMouse() { content.style['pointer-events'] = 'auto'; }
     function onScroll() { content.style['pointer-events'] = 'none'; }
 
+    // AFTER 4 SECONDS DISPLAY TOAST WITH INSTRUCTIONS
     setTimeout(() => {
-        toast.classList.add('active')
+        toast.classList.add('toast-active')
     }, 4000)
     
     toastClose.addEventListener('click', () => {
-        toast.classList.remove('active')
+        toast.classList.remove('toast-active')
     })
 
     
 })
 
+// FUNCTION FOR SETTING THE CURRENT NAVBAR LINK
+function setActiveLink(linkElement) {
+    pageLinks.forEach((link, idx) => {
+        if(idx !== current_index) {
+            link.classList.remove('link-active')
+
+        }
+    })
+    linkElement.classList.add('link-active')
+}
+
+// SET DEFAULT SCHEDULE ITEM
 iframes[0].classList.add('iframe-active')
 
-
-
-
-// const main = document.getElementById('main')
-// const sections = document.querySelectorAll('.slider-item')
-// const slider = document.getElementById('slider')
-// let sliderWidth
-// let imageWidth
-// let sectionWidth
-// let current = 0
-// let target = 0
-// let ease = 0.07
-
-
-// function lerp(start, end, t) {
-//     return start * (1-t) + end * t
-// } 
-
-// function setTransform(element, transform) {
-//     element.style.transform = transform
-// }
-
-// function init() {
-//     sliderWidth = slider.getBoundingClientRect().width
-//     sectionWidth = sliderWidth / sections.length
-//     document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
-// }
-
-// function animate() {
-//     current = parseFloat(lerp(current, target, ease)).toFixed(2)
-//     target = window.scrollY
-//     setTransform(slider, `translateX(-${current}px)`)
-//     // animateImages()
-//     requestAnimationFrame(animate)
-// }
-
-// function animateImages() {
-//     let ratio = current / imageWidth
-//     let intersectionRatioValue
-
-//     images.forEach((image, idx) => {
-//         intersectionRatioValue = ratio - (idx * 0.7)
-//         setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
-//     })
-// }
-
-// init()
-// animate()
